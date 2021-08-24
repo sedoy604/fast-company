c8f0cc2f932c3320fcc6c2f37371de7cfa7747b5
sedoy604 <54064543+sedoy604@users.noreply.github.com>
2021-08-24 20:07:23 +0300

    final state of app

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -2,15 +2,57 @@ import React, { useState } from "react";
 import api from "../api";
 
 const Usrs = () => {
+  const getClasses = (personId) => {
+    let bootClass = "m-1 badge bg-";
+    if (personId.color === "primary") {
+      bootClass += "primary";
+    } else if (personId.color === "secondary") {
+      bootClass += "secondary";
+    } else if (personId.color === "info") {
+      bootClass += "secondary";
+    } else if (personId.color === "success") {
+      bootClass += "success";
+    } else if (personId.color === "danger") {
+      bootClass += "danger";
+    } else if (personId.color === "info") {
+      bootClass += "info";
+    } else if (personId.color === "dark") {
+      bootClass += "dark";
+    }
+    return bootClass;
+    // personId.color === "primary"
+    //   ? (bootClass += "primary")
+    //   : console.log(bootClass);
+    // personId.color === "secondary"
+    //   ? (bootClass += "secondary")
+    //   : console.log(bootClass);
+    // personId.color === "success"
+    //   ? (bootClass += "success")
+    //   : console.log(bootClass);
+    // personId.color === "danger"
+    //   ? (bootClass += "danger")
+    //   : console.log(bootClass);
+    // personId.color === "danger"
+    //   ? (bootClass += "info")
+    //   : console.log(bootClass);
+    // personId.color === "danger"
+    //   ? (bootClass += "dark")
+    //   : console.log(bootClass);
+  };
+
   const [users, setUsers] = useState(api.users.fetchAll());
   const handleDelete = (userId) => {
-    setUsers(users.filter((x) => x._id !== userId));
+    setUsers(users.filter((el) => el._id !== userId));
   };
   const renderRow = (array) => {
     return array.map((item) => (
       <tr key={item._id}>
         <td>{item.name}</td>
-        <td>{item.qualities.map((el) => el.name)}</td>
+        <td>
+          {item.qualities.map((el) => (
+            <span className={getClasses(el)}>{el.name}</span>
+          ))}
+        </td>
         <td>{item.profession.name}</td>
         <td>{item.completedMeetings}</td>
         <td>{item.rate}</td>
